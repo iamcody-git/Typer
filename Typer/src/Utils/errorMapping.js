f5f1f0ffda33fdddf347b7b022de5ce588ba5af8
iamcody-git <iamcody.github@gmail.com>
2025-01-15 10:46:23 +0545

    error handler file

---
+++ Typer/src/Utils/errorMapping.js
@@ -0,0 +1,8 @@
+export const errorMapping ={
+    'auth/user-not-found': 'User not found',
+    'auth/wrong-password': 'Wrong password',
+    'auth/email-already-in-use': 'Email already in use',
+    'auth/weak-password': 'Password is too weak must be atleast 6 characters',
+    'auth/invalid-email': 'Invalid email',
+    'auth/too-many-requests': 'Too many requests, please try again later',
+}
